954dbed94d7a8e271dc899a22058d5f398af18d3
fa18swiss <fa18swiss@users.noreply.github.com>
2015-04-28 11:21:04 +0200

    build without library with Visual Studio

--- GpioMpsse.cpp
+++ GpioMpsse.cpp
@@ -1,10 +1,29 @@
 #include "GpioMpsse.h"
 #include <iostream>
 
+#ifdef _MSC_VER
+int PinHigh(struct mpsse_context *, int) 
+{
+	return 0;
+}
+int PinState(struct mpsse_context *, int, int)
+{
+	return 0;
+}
+int PinLow(struct mpsse_context *, int)
+{
+	return 0;
+}
+int Close(struct mpsse_context *)
+{
+	return 0;
+}
+#else
 extern "C"
 {
 #include <mpsse.h>
 }
+#endif
 
 using std::cout;
 using std::endl;
@@ -12,22 +31,21 @@ using std::endl;
 
 GpioMpsse::GpioMpsse()
 {
+#ifndef _MSC_VER
 	io = MPSSE(GPIO, 0, 0);
 	if (io) {
 		cout << "IO pass" << endl;
-	}
-	else {
+	} else {
 		cout << "IO not pass" << endl;
 	}
 
 	if (io->open) {
 		cout << "IO open" << endl;
-	}
-	else {
+	} else {
 		cout << "IO not open" << endl;
 	}
+#endif
 }
-
 
 GpioMpsse::~GpioMpsse()
 {
@@ -44,5 +62,5 @@ void GpioMpsse::SetPin(int pin, bool isHigh)
 	else {
 		write = PinLow(io, pin);
 	}
-	cout << "State:  << " << PinState(io, pin, -1) << " / " << write << endl;
+	cout << "State: " << PinState(io, pin, -1) << " / " << write << endl;
 }
