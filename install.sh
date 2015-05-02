ef67ad76093fe9d7e52772ebf972e5dbf0084488
fa18swiss <fa18swiss@users.noreply.github.com>
2015-05-02 18:43:05 +0200

    add missing /

--- install.sh
+++ install.sh
@@ -8,25 +8,25 @@ cd $DIR
 sudo apt-get update
 
 #insall dependencies
-yes  sudo apt-get install build-essential cmake libusb-1.0-0-dev swig python-dev libconfuse-dev libboost-all-dev libftdi-dev python-ftdi screen
+yes | sudo apt-get install build-essential cmake libusb-1.0-0-dev swig python-dev libconfuse-dev libboost-all-dev libftdi-dev python-ftdi screen
 
 #libftdi1-1.2
 echo libftdi1-1.2
 cd libftdi1-1.2
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=usr ..
+cmake -DCMAKE_INSTALL_PREFIX="/usr/" ../
 time make
 sudo make install
-cd ....
+cd ../..
 
 #ibmpsse-1.3
 echo ibmpsse-1.3
-cd libmpsse-1.3src
-.configure
+cd libmpsse-1.3/src
+./configure
 make
 sudo make install
-cd ....
+cd ../..
 
 #enum34-1.0.4
 echo enum34-1.0.4
@@ -40,5 +40,4 @@ cd ServerCpp
 make
 cd ..
 
-echo install  build finish
-
+echo install / build finish
